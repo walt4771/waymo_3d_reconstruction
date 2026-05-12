6d41aeb6733ad11d836fb9acc1f536820581e3cb
walt4771 <42925880+walt4771@users.noreply.github.com>
2026-05-12 22:43:47 +0900

    Use sudo for Python installation and cleanup
    Updated installation commands for Python and CUDA.

--- wsl_debian13_init.sh
+++ wsl_debian13_init.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz;tar -xf Python-3.12.0.tgz;cd Python-3.12.0
 ./configure --enable-optimizations
-make install
-rm -rf Python-3.12.0.tgz
-rm -rf Python-3.12.0
+sudo make install
+sudo rm -rf Python-3.12.0.tgz
+sudo rm -rf Python-3.12.0
 
 sudo apt install -y wget libxml2 git
 wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda_12.4.1_550.54.15_linux.run
