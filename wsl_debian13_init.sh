bbe78032e067aa4f8185754ae347fec5e1913c1d
walt4771 <42925880+walt4771@users.noreply.github.com>
2026-05-12 22:40:37 +0900

    Revise WSL Debian init script for Python and CUDA
    Updated installation commands to include additional dependencies and CUDA installation.

--- wsl_debian13_init.sh
+++ wsl_debian13_init.sh
@@ -1,5 +1,13 @@
 sudo apt update
-sudo apt install -y wget libxml2 build-essential python3.12 python3-pip python3-venv git
+
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz;tar -xf Python-3.12.0.tgz;cd Python-3.12.0
+./configure --enable-optimizations
+make install
+rm -rf Python-3.12.0.tgz
+rm -rf Python-3.12.0
+
+sudo apt install -y wget libxml2 git
 wget https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda_12.4.1_550.54.15_linux.run
 sudo sh cuda_12.4.1_550.54.15_linux.run --silent --toolkit --override
 echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
